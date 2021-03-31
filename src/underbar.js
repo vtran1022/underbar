0a55b096b2813467fa68306a499b9d93d06aef9c
Vy Tran <vtran1022@Gmail.com>
2021-03-30 18:36:09 -0700

    implement last changes - unable to use ? operator, will return to it, commented out starting code for it

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,16 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    // return n === undefined ? array[array.length - 1] : array.slice(n - 1, array.length);
+    if (n === undefined) {
+      return array[array.length - 1];
+    } else if (n > array.length) {
+      return array;
+    } else if (n === 0) {
+      return [];
+    } else {
+      return array.slice(n - 1, array.length);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
