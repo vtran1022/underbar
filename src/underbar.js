deb07eacc8ce38555ac03ff07741f155341259e7
Vy Tran <vtran1022@Gmail.com>
2021-04-03 21:34:29 -0700

    implementing changes to delay func

--- src/underbar.js
+++ src/underbar.js
@@ -425,6 +425,13 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var argumentsList = [];
+    for (var i = 2; i < arguments.length; i++) {
+      argumentsList.push(arguments[i]);
+    }
+    setTimeout(function() {
+      return func.apply(null, argumentsList);
+    }, wait);
   };
 
 
