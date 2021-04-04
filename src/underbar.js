434ee6a85415dc1d7eb5bcd4af5923231b6eb178
Vy Tran <vtran1022@Gmail.com>
2021-04-03 22:11:53 -0700

    implement shuffle func changes

--- src/underbar.js
+++ src/underbar.js
@@ -446,6 +446,14 @@
   // input array. For a tip on how to make a copy of an array, see:
   // http://mdn.io/Array.prototype.slice
   _.shuffle = function(array) {
+    var newArray = array.slice();
+    for (var i = 0; i < array.length; i++) {
+      var randomIndex = Math.floor(Math.random() * i);
+      var shiftedValue = newArray[i];
+      newArray[i] = newArray[randomIndex];
+      newArray[randomIndex] = shiftedValue;
+    }
+    return newArray;
   };
 
 
